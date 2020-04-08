f51c2ae461b9baefdc9b94a09a4ebacb0d8c6f91
tavor <mf.galesic@gmail.com>
2020-04-08 12:05:35 -0700

    Modify merge sort to support cases based on and inspired by SO answer
     - modified combination algorithm to do in-place shift when an element
       is less than one before
    
    Stack Overflow answer https://stackoverflow.com/a/15124092/1456621

--- MergeSortDriver.java
+++ MergeSortDriver.java
@@ -6,10 +6,24 @@ public class MergeSortDriver {
 		
 		int[] input = new int[] { 7, -1, 2, 5, 11 };
 		program.mergeSort(input);
-
-		program.printArray(input);
 		
+		int[] oneElement = new int[1];
+		program.mergeSort(oneElement);
+
+		int[] allSame = new int[] { 1, 1 };
+		program.mergeSort(allSame);
+
+		int[] reverseSorted = new int[] { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+		program.mergeSort(reverseSorted);
+
+		int[] duplicates = new int[] { 10, 9, 8, 10, 6, 5, 4, 3, 2, 1 };
+		program.mergeSort(duplicates);
+
 		assert Arrays.equals(new int[] { -1, 2, 5, 7, 11 }, input);
+		assert Arrays.equals(new int[1], oneElement);
+		assert Arrays.equals(new int[] { 1, 1 }, allSame);
+		assert Arrays.equals(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }, reverseSorted);
+		assert Arrays.equals(new int[] { 1, 2, 3, 4, 5, 6, 8, 9, 10, 10}, duplicates);
 	}
 
 	public void mergeSort(int[] a) {
@@ -28,16 +42,20 @@ public class MergeSortDriver {
 			int splitPoint = a.length % 2 == 0 ? a.length / 2 - 1 : a.length / 2;
 			mergeSort(a, 0, splitPoint);
 			mergeSort(a, splitPoint + 1, a.length - 1);
-
-			for (int i = 0, j = splitPoint + 1; i <= splitPoint && j < a.length;) {
-				if (a[i] < a[j]) {
-					i++;
-				} else {
-					int t = a[i];
-					a[i] = a[j];
-					a[j] = t;
-					j++;
+		
+			int j = splitPoint + 1, i = 0;
+			while (j < a.length) {
+				for (;i < j; i++) {
+					if (a[i] > a[j]) {
+						break;
+					}
 				}
+				int t = a[j], k = j;
+				for (;k > i; k--) {
+					a[k] = a[k - 1];
+				}
+				a[k] = t;
+				j++; i++;
 			}
 		}
 	}	
@@ -51,22 +69,26 @@ public class MergeSortDriver {
 
 	private void mergeSort(int[] a, int start, int end) {
 		int length = end - start + 1;
-		if(length > 2) {
+		if (length > 2) {
 			int splitPoint = length % 2 == 0 ? length / 2 - 1: length / 2;
 
 			mergeSort(a, start, start + splitPoint);
 			mergeSort(a, start + splitPoint + 1, end);
-					
-			for (int i = start, j = splitPoint + 1; i <= splitPoint && j <= end;) {
-				if (a[i] < a[j]) {
-					i++;
-				} else {
-					int t = a[i];
-					a[i] = a[j];
-					a[j] = t;
-					j++;
+
+			int j = start + splitPoint + 1, i = start;
+			while (j < end + 1) {
+				for (;i < j; i++) {
+					if (a[i] > a[j]) {
+						break;
+					}
 				}
-			}	
+				int t = a[j], k = j;
+				for (;k > i; k--) {
+					a[k] = a[k - 1];
+				}
+				a[k] = t;
+				j++; i++;
+			}
 		} else if (length == 2 && a[start] > a[end]) {
 			int t = a[start];
 			a[start] = a[end];
