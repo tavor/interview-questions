b4358559a5656bfca75a62475a358731ae22a66c
tavor <mf.galesic@gmail.com>
2020-04-25 10:21:54 -0700

    Re-implement insertion sort

--- InsertionSortMain.java
+++ InsertionSortMain.java
@@ -6,49 +6,61 @@ public class InsertionSortMain {
 	public static void main(String[] args) {
 		InsertionSortMain program = new InsertionSortMain();
 		
-		List<Integer> sortedList1 = program.insertionSort(new int[]{3, 2, 1});		
+		int[] a1 = new int[]{3, 2, 1};
+		program.insertionSort(a1);		
 	
-		List<Integer> sortedList2 = program.insertionSort(new int[]{1, 2, -1});
+		int[] a2 = new int[]{1, 2, -1};
+		program.insertionSort(a2);
 
-		List<Integer> sortedList3 = program.insertionSort(new int[]{1, 101, 100});
+		int[] a3 = new int[]{1, 101, 100};
+		program.insertionSort(a3);
 
-		List<Integer> sortedList4 = program.insertionSort(new int[]{1, 2, 1, 3});
+		int[] a4 = new int[]{1, 2, 1, 3};
+		program.insertionSort(a4);
 
-		List<Integer> sortedList5 = program.insertionSort(new int[]{0, 1, 2, 3});
+		int[] a5 = new int[]{0, 1, 2, 3};
+		program.insertionSort(a5);
 
-		List<Integer> sortedList6 = program.insertionSort(new int[]{12, 11, 14, 15, 6});		
+		int[] a6 = new int[]{12, 11, 14, 15, 6};
+		program.insertionSort(a6);		
 		
-		List<Integer> sortedList7 = program.insertionSort(new int[]{4, 3, 2, 10, 12, 1, 5, 6});
-		System.out.println(sortedList1.toString());
-		System.out.println(sortedList2.toString());
-		System.out.println(sortedList3.toString());
-		System.out.println(sortedList4.toString());
-		System.out.println(sortedList5.toString());
-		System.out.println(sortedList6.toString());
-		System.out.println(sortedList7.toString());
+		int[] a7 = new int[]{4, 3, 2, 10, 12, 1, 5, 6};
+		program.insertionSort(a7);
+
+		program.printArray(a1);
+		program.printArray(a2);
+		program.printArray(a3);
+		program.printArray(a4);
+		program.printArray(a5);
+		program.printArray(a6);
+		program.printArray(a7);
 	}
-	
-	/* Takes elements from list one by one and inserting them in
-         * in their correct position into a new sorted list
-         */
-	public List<Integer> insertionSort(int[] list) {
-		if (list.length == 0) {
-			return new ArrayList<Integer>();
-		}
-		
-		ArrayList<Integer> sortedArray = new ArrayList<Integer>();
-		sortedArray.add(list[0]);
-			
-		for (int i = 1; i < list.length; i++) {
-			int sortedArrayIndex = 0;
 
-			while (sortedArrayIndex < sortedArray.size() &&
-			       list[i] < sortedArray.get(sortedArrayIndex)) {
-				sortedArrayIndex++;
-			}
-			
-			sortedArray.add(sortedArrayIndex, list[i]);
+	private void printArray(int[] a) {
+		for (Integer i : a) {
+			System.out.print(i + " ");
 		}
-		return sortedArray;
-	}	
+		System.out.println();
+	}
+
+	public void insertionSort(int[] a) {
+		int i = 0;
+		while (i < a.length) {
+			for (int j = 0; j < i; j++) {
+				if (a[i] < a[j]) {
+					int t = a[i];
+					shift(a, i, j);
+					a[j] = t;
+				}
+			}
+			i++;
+		}
+	}
+
+	private void shift(int[] a, int i, int j) {
+		while (i > j) {
+			a[i] = a[i - 1];
+			i--;
+		}
+	}
 }
